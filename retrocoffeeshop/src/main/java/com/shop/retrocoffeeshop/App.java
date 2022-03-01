416e42dfdcfb1944c51a55b4b41597258192c378
swetha2703 <swethapurple2003@gmail.com>
2022-03-01 10:51:03 -0500

    changesmade

--- retrocoffeeshop/src/main/java/com/shop/retrocoffeeshop/App.java
+++ retrocoffeeshop/src/main/java/com/shop/retrocoffeeshop/App.java
@@ -2,6 +2,6 @@ package com.shop.retrocoffeeshop;
 
 public class App {
   public static void main(String[] args) {
-    System.out.println("Hello World!!!!Pavi");
+    System.out.println("Hello World!!!!");
   }
 }
