d8281a6be7fdac1dd03ffd8b730d72e832b09d57
pavi06 <pavi06saishivu@gmail.com>
2022-03-01 14:13:26 +0530

    order created

---
+++ retrocoffeeshop/src/main/java/com/shop/retrocoffeeshop/entity/order.java
@@ -0,0 +1,20 @@
+package com.shop.retrocoffeeshop.entity;
+
+public class order {
+	int orderId;
+	double totalOrderAmount;
+	
+	public int getOrderId() {
+		return orderId;
+	}
+	public void setOrderId(int orderId) {
+		this.orderId = orderId;
+	}
+	public double getTotalOrderAmount() {
+		return totalOrderAmount;
+	}
+	public void setTotalOrderAmount(double totalOrderAmount) {
+		this.totalOrderAmount = totalOrderAmount;
+	}
+	
+}
