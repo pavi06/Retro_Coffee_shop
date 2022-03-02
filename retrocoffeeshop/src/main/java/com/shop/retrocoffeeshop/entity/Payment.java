da458dc528d995c5fcc4bb44570f31a0e4d8983b
pavi06 <pavi06saishivu@gmail.com>
2022-03-02 11:05:31 +0530

    Payment changed

--- retrocoffeeshop/src/main/java/com/shop/retrocoffeeshop/entity/Payment.java
+++ retrocoffeeshop/src/main/java/com/shop/retrocoffeeshop/entity/Payment.java
@@ -1,8 +1,5 @@
 package com.shop.retrocoffeeshop.entity;
 
-public class Payment {
-	package com.onlineshopping.entity;
-
 	import javax.persistence.Entity;
 	import javax.persistence.GeneratedValue;
 	import javax.persistence.GenerationType;
@@ -18,23 +15,18 @@ public class Payment {
 		@SequenceGenerator(name="payment_seq",initialValue = 1,allocationSize = 1)
 		@GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "payment_seq")
 		int paymentId;
-		TransactionType transactionType;
 
 		@OneToOne
 		@JoinColumn(name="orderId")
-		Order order;
-
-		
-		
+		Order order;		
 		
 		public Payment() {
 			super();
 		}
 
-		public Payment(int paymentId, TransactionType transactionType, Order order) {
+		public Payment(int paymentId, Order order) {
 			super();
 			this.paymentId = paymentId;
-			this.transactionType = transactionType;
 			this.order = order;
 		}
 
@@ -44,14 +36,6 @@ public class Payment {
 
 		public void setPaymentId(int paymentId) {
 			this.paymentId = paymentId;
-		}
-
-		public TransactionType getTransactionType() {
-			return transactionType;
-		}
-
-		public void setTransactionType(TransactionType transactionType) {
-			this.transactionType = transactionType;
 		}
 
 		public Order getOrder() {
