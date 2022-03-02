f4df76e1f3b59b6f40e8a29eb4d9a9b8e0c7365b
Valliammai <Valliammai@192.168.3.103>
2022-03-02 19:21:43 +0530

    Orderedited

--- retrocoffeeshop/src/main/java/com/shop/retrocoffeeshop/repository/OrderRepo.java
+++ retrocoffeeshop/src/main/java/com/shop/retrocoffeeshop/repository/OrderRepo.java
@@ -1,5 +1,14 @@
-package com.shop.retrocoffeeshop.repository;
+package com.shop.retrocoffeeshop.repository; 
+
+import java.util.List; 
+import com.onlineshopping.entity.Order;
+import com.onlineshopping.entity.User;
 
 public interface OrderRepo {
+	Order createOrder();
+	void cancelOrder(Order order);
+	Order repeatOrder(Order order);
+	Order viewOrderDetails(int orderId);
+	List<Order> getOrdersByUserId(int userId);
 
 }
--- retrocoffeeshop/src/main/java/com/shop/retrocoffeeshop/repository/OrderRepoImpl.java
+++ retrocoffeeshop/src/main/java/com/shop/retrocoffeeshop/repository/OrderRepoImpl.java
@@ -1,5 +1,51 @@
 package com.shop.retrocoffeeshop.repository;
+import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query; 
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.shop.retrocoffeeshop.entity.Cart;
+import com.shop.retrocoffeeshop.entity.Order;
+import com.shop.retrocoffeeshop.entity.OrderStatus; 
+import com.shop.retrocoffeeshop.entity.User;
+
+@Repository
 public class OrderRepoImpl implements OrderRepo {
+@PersistenceContext
+EntityManager em;
+	
+@Override
+@Transactional
+	public Order createOrder() {
+		Order order=new Order();
+		order.setTotalOrderAmount(0); 
+		return em.merge(order);
+		
+	}
 
+@Override
+public void cancelOrder(Order order) {
+	em.remove(order);
+} 
+@Override
+public Order repeatOrder(Order order) {
+	return null;
+}
+@Override
+public Order viewOrderDetails(int orderId) {
+	return em.find(Order.class, orderId);
+}
+@Override
+public List<Order>
+getOrdersByUserId(int userId){
+	//TODO Auto-generated method stub
+	String jpql="select o from Order o where o.user_id=:uid";
+	Query query=em.createQuery(jpql);
+	query.setParameter("uid", userId); 
+	return query.getResultList();
+}
 }
