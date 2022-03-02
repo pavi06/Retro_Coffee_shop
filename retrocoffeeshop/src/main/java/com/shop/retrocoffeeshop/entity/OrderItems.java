39c743545d5e7b1991cd822755f36988d1722d82
pavi06 <pavi06saishivu@gmail.com>
2022-03-02 11:46:47 +0530

    orderItems class defined

--- retrocoffeeshop/src/main/java/com/shop/retrocoffeeshop/entity/OrderItems.java
+++ retrocoffeeshop/src/main/java/com/shop/retrocoffeeshop/entity/OrderItems.java
@@ -1,5 +1,60 @@
 package com.shop.retrocoffeeshop.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tbl_orderItems")
+
 public class OrderItems {
+	
+	@Id
+	@SequenceGenerator(name="orderItems_seq")
+	@GeneratedValue(strategy=GenerationType.SEQUENCE,generator="orderItems_seq")
+	
+	int orderItemId;
+	int orderItemQuantity;
+	
+	@OneToOne
+	@JoinColumn(name="itemId")
+	Items item;
+
+	Order order;
+	
+	public int getOrderItemId() {
+		return orderItemId;
+	}
+	public void setOrderItemId(int orderItemId) {
+		this.orderItemId = orderItemId;
+	}
+	public int getOrderItemQuantity() {
+		return orderItemQuantity;
+	}
+	public void setOrderItemQuantity(int orderItemQuantity) {
+		this.orderItemQuantity = orderItemQuantity;
+	}
+	
+	public Items getItem() {
+		return item;
+	}
+	public void setItem(Items item) {
+		this.item = item;
+	}
+	public Order getOrder() {
+		return order;
+	}
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+	
+	
+	
+	
 
 }
